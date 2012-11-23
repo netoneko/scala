3f68365dc72716e3b3619e1e1e6d68d52e779de0
Kirill Maksimov <netoneko@gmail.com>
2012-11-23 17:32:24 +0200

    curry

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,7 +1,29 @@
 object Hello {
   def main(args: Array[String]) {
-    val addOne = (x: Int) => x + 1
+    val add = (x: Int, y: Int) => x + y
 
-    println(addOne(2))
+    println(add(2, 3))
+
+    val add2 = add(2, _:Int)
+
+    println(add2(3))
+
+    def realPower(base: Int, power: Int): Int = {
+      if (power == 0) 1
+      else if (power == 1) base
+      else base * realPower(base, power - 1)
+    }
+
+    println(realPower(2, 3))
+
+    def pow(base: Int)(power: Int) = {
+      realPower(base, power)
+    }
+
+    println(pow(2)(3))
+
+    val square = realPower(_: Int, 2)
+
+    println(square(7))
   }
 }
