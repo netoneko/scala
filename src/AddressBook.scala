65b7f7cb1ce8c04b1ced5f877b7df66bedbf6911
Kirill Maksimov <netoneko@gmail.com>
2013-01-13 15:47:12 +0200

    add

--- src/AddressBook.scala
+++ src/AddressBook.scala
@@ -1,25 +1,50 @@
+import collection.mutable
+
 object AddressBook {
-  case class Command(name: String, apply: (Array[String]) => String);
 
-  val commands = Array(
-    Command("echo", { input: Array[String] =>
-      input.mkString(" ")
-    })
-  )
+  val entries = mutable.HashMap[String, String]()
 
-  def main(args: Array[String]) {
-    println(commands.head.apply _)
+  def list(input: Array[String]): String = {
+    entries.map { entry =>
+      Array(entry._1, "\t<", entry._2, ">").mkString
+    }.mkString("\n")
+  }
 
+  def add(input: Array[String]): String = {
+    val name = input.dropRight(1).mkString(" ")
+    val email = input.last
+    entries.put(name, email)
+
+    "Entry added: " + name + " <" + email + ">"
+  }
+
+  def echo(input: Array[String]): String = {
+    "Could not parse command: " + input.mkString(" ")
+  }
+
+  def handleInput {
     print("> ")
 
     for (ln <- io.Source.stdin.getLines) {
-      val result = ln match {
+      val tokens = ln.split(" ")
 
-        case _ => commands.head.apply(ln.split(" "))
+      val command = tokens.head match {
+        case "list" => list _
+        case "add" => add _
+        case _ => echo _
       }
 
-      println(result)
+      val output = command(tokens.drop(1))
+      println(output)
       print("> ")
     }
+
+  }
+
+  def main(args: Array[String]) {
+    entries += ("Spider Jerusalem" -> "spidey@theword.com")
+    entries += ("Mitchell Royce" -> "mitch@theword.com")
+
+    handleInput
   }
 }
