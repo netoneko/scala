57dc22e07d9d449c15e6466d00b2e4763b6baec7
Kirill Maksimov <netoneko@gmail.com>
2013-01-13 17:16:01 +0200

    anonymous functions

--- src/AddressBook.scala
+++ src/AddressBook.scala
@@ -3,50 +3,39 @@ import collection.mutable
 object AddressBook {
   val entries = mutable.HashMap[String, String]()
 
-  def list(input: Array[String]): String = {
-    entries.map {
-      entry =>
-        Array(entry._1, "\t<", entry._2, ">").mkString
-    }.mkString("\n")
-  }
-
-  def add(input: Array[String]): String = {
-    val name = input.dropRight(1).mkString(" ")
-    val email = input.last
-    entries.put(name, email)
-
-    "Entry added: %s <%s>".format(name, email)
-  }
-
-  def del(input: Array[String]): String = {
-    val key = input.mkString(" ")
-
-    val value = entries.remove(key)
-    if (value != None) {
-      "Entry removed: %s <%s>".format(key, value.get)
-    }
-    else {
-      "Entry not found"
-    }
-  }
-
-  def exit(input: Array[String]): String = {
-    "" + System.exit(0)
-  }
-
-  def echo(input: Array[String]): String = {
-    "Could not parse command: " + input.mkString(" ")
-  }
-
   def handleInput(input: String): String = {
     val tokens = input.split(" ")
 
     val command = tokens.head match {
-      case "list" => list _
-      case "add" => add _
-      case "del" => del _
-      case "^D" => exit _
-      case _ => echo _
+      case "list" => {
+        input: (Array[String]) =>
+          entries.map {
+            entry =>
+              Array(entry._1, "\t<", entry._2, ">").mkString
+          }.mkString("\n")
+      }
+      case "add" => {
+        input: (Array[String]) =>
+          val name = input.dropRight(1).mkString(" ")
+          val email = input.last
+          entries.put(name, email)
+          "Entry added: %s <%s>".format(name, email)
+      }
+      case "del" => {
+        input: (Array[String]) =>
+          val key = input.mkString(" ")
+          val value = entries.remove(key)
+          if (value != None)
+            "Entry removed: %s <%s>".format(key, value.get)
+          else
+            "Entry not found"
+      }
+      case "^D" => {
+        input: (Array[String]) => "" + System.exit(0)
+      }
+      case _ => {
+        input: (Array[String]) => "Could not parse command: " + input.mkString(" ")
+      }
     }
 
     command(tokens.drop(1))
