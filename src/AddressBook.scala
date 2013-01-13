f33d4e5ebda435d588cde96744883caa2768775d
Kirill Maksimov <netoneko@gmail.com>
2013-01-13 15:58:50 +0200

    del

--- src/AddressBook.scala
+++ src/AddressBook.scala
@@ -1,7 +1,6 @@
 import collection.mutable
 
 object AddressBook {
-
   val entries = mutable.HashMap[String, String]()
 
   def list(input: Array[String]): String = {
@@ -18,6 +17,18 @@ object AddressBook {
     "Entry added: " + name + " <" + email + ">"
   }
 
+  def del(input: Array[String]): String = {
+    val key = input.mkString(" ")
+
+    val value = entries.remove(key)
+    if (value != None) {
+      "Entry removed: " + key + " <" + value + ">";
+    }
+    else {
+      "Entry not found"
+    }
+  }
+
   def echo(input: Array[String]): String = {
     "Could not parse command: " + input.mkString(" ")
   }
@@ -31,6 +42,7 @@ object AddressBook {
       val command = tokens.head match {
         case "list" => list _
         case "add" => add _
+        case "del" => del _
         case _ => echo _
       }
 
